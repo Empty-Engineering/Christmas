124b2252b9f4b14b7f912da7d8d9cb2cd488910b
empty-engineering <lukasmrobin@gmail.com>
2021-10-23 16:58:33 -0400

    Initial Commit

---
+++ Christmas/ChristmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasApp.swift
+//  Christmas
+//
+//  Created by Lukas Robin on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChristmasTests/ChristmasTests.swift
@@ -0,0 +1,33 @@
+//
+//  ChristmasTests.swift
+//  ChristmasTests
+//
+//  Created by Lukas Robin on 23.10.2021.
+//
+
+import XCTest
+@testable import Christmas
+
+class ChristmasTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
