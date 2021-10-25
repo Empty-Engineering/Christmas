b7f68e352caa5d85d974ea4adc599135ea068d7a
empty-engineering <lukasmrobin@gmail.com>
2021-10-24 23:03:05 -0400

    ;;;

--- Christmas/ContentView.swift
+++ Christmas/ContentView.swift
@@ -1,15 +1,30 @@
 //
 //  ContentView.swift
-//  Christmas
+//  Christmas Countdown
 //
-//  Created by Lukas Robin on 23.10.2021.
+//  Created by Lukas Robin on 22.10.2021.
 //
 
 import SwiftUI
-
+//get the current year
+func get_year() -> Int {
+    let date = Date()
+    let calendar = Calendar.current
+    let components = calendar.dateComponents([.year], from: date)
+    let year = components.year
+    return year!
+}
+//calculate days until christmas
+func daysUntilChristmas() -> Int {
+    let today = Date()
+    let christmas = Calendar.current.date(from: DateComponents(year: get_year(), month: 12, day: 25))!
+    let components = Calendar.current.dateComponents([.day], from: today, to: christmas)
+    return components.day!
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        let daysUT: Int = daysUntilChristmas();
+        Text("\(daysUT) days until Christmas!")
             .padding()
     }
 }
